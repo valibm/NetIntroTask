5e521a335a64b0627dc15de8f8ae294247f532ae
valibm <valibm@code.edu.az>
2022-06-13 21:27:34 +0400

    first commit

---
+++ NetIntroTask/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace NetIntroTask.Controllers
+{
+    public class HomeController :Controller
+    {
+
+        List<int> _numbers = new List<int>()
+        {
+            1, 3, 7, 9, 8, 15, 19, 23, 32, 37
+        };
+
+        [Route("home/getnumber/{number}")]
+        public IActionResult GetNumber(int number)
+        {
+            var isExist = _numbers.Exists(n => n == number);
+            if (isExist)
+            {
+                int myNumber = _numbers.Find(n => n == number);
+                return View(model: myNumber.ToString());
+            }
+            else
+            {
+                return View(model: "Number could not be found.");
+            }
+        }
+
+        public IActionResult GetPerson()
+        {
+            var person = new { firstname = "Vali", surname = "Mustafayev", age = 21};
+
+            return Json(person);
+        }
+
+        public IActionResult SayHello()
+        {
+            return Content("Salam");
+        }
+
+        
+    }
+}
